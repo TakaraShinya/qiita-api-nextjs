a98065fbc741a67197cc999a28d23206a4ca687f
吉井駿一 <syun_syun01@icloud.com>
2022-07-10 10:21:48 +0900

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# next-lint-template-tailwind
+
+Qiita で説明した Next.js の Lint 設定のテンプレートプロジェクトです
+
+Tailwind CSS にも対応しています。
+
+## Qiita
+
+https://qiita.com/ganja_tuber/items/95507e658ecfef7e9457
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  plugins: [],
+  theme: {
+    extend: {},
+  },
+};
