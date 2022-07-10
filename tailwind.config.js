ea045de4f7ff8b8a665b8ea182d60ca364ab5dbe
吉井駿一 <syun_syun01@icloud.com>
2022-07-10 14:27:21 +0900

    :wrench: 設定ファイルの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   plugins: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        qiita: "#59bb0c",
+      },
+    },
   },
 };
