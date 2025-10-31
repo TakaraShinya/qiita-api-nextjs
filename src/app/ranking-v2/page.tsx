61ce24335f48d077ed26a9b1311ad1730112ec61
takara_shinya　[Administrator] <takara_shinya@ca-adv.co.jp>
2025-10-31 10:21:55 +0900

    fix: production build時のエラー解消

--- src/app/ranking-v2/page.tsx
+++ src/app/ranking-v2/page.tsx
@@ -1,14 +1,9 @@
-import { Suspense } from "react";
-
-import Loading from "./components/loading";
 import RankingContent from "./components/ranking-content";
 
 const Page = async () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-900 via-blue-900 to-indigo-900">
-      <Suspense fallback={<Loading />}>
-        <RankingContent />
-      </Suspense>
+      {await RankingContent()}
     </div>
   );
 };
